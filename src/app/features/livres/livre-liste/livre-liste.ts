1a2be2cff0ca6ac9e3ee1391310d3d9b1dd9a2fe
nolanjean <nobodyplayeur@gmail.com>
2026-09-22 14:02:21 +0200

    feat(livres): afficher la liste des livres depuis l'api

--- src/app/features/livres/livre-liste/livre-liste.ts
+++ src/app/features/livres/livre-liste/livre-liste.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
+import { LivreService } from '../livre';
+import { Livre } from '../../../core/models/livre';
 
 @Component({
   imports: [],
@@ -6,4 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './livre-liste.css',
   templateUrl: './livre-liste.html',
 })
-export class LivreListe {}
+export class LivreListe implements OnInit {
+  private livreService = inject(LivreService);
+
+  protected readonly livres = signal<Livre[]>([]);
+  protected readonly chargement = signal(true);
+  protected readonly erreur = signal<string | null>(null);
+
+  ngOnInit(): void {
+    this.livreService.getLivres().subscribe({
+      next: (page) => {
+        this.livres.set(page.content);
+        this.chargement.set(false);
+      },
+      error: (err) => {
+        this.erreur.set('Impossible de charger les livres.');
+        this.chargement.set(false);
+      },
+    });
+  }
+}
